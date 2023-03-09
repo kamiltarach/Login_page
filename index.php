29688db6feab74706baf0cefa128f63141d08561
Kamil Tarach <kamiltarach.kt@gmail.com>
2023-03-09 12:00:51 +0100

    1st commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login page - main page</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="icon" href="icon.png">
+</head>
+<body>
+    <div class="header">
+        <p>Witajcie na prostej stronie do logowania</p>
+        <ul>
+            <li><a href="">Zarejestruj się</a></li>
+            <li><a href="">Zaloguj się</a></li>
+            <li><a href="">Pomoc</a></li>
+        </ul>
+    </div>
+    <div class="main_content">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde illum repellat beatae nam necessitatibus commodi, nulla neque voluptatum id, a quos repellendus accusamus ducimus vero ea aperiam doloremque quidem! Ipsum?</p>
+    </div>
+    <div class="footer">
+        <p>Autor: Kamil Tarach | Email: <a href="mailto:kamiltarach.kt@gmail.com">kamiltarach.kt@gmail.com</a></p>
+    </div>
+</body>
+</html>
